918254d28e0a9da6961eb519320e161176f8ec53
sineen <sineen.nawatha@mail.huji.ac.il>
2019-04-13 16:56:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/selfchat_rv/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.selfchat_rv;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText input;
+    RecyclerView recyclerView;
+    MyAdapter ViewAdapter;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.RV);
+        input = (EditText) findViewById(R.id.plain_text_input);
+        button = (Button) findViewById(R.id.button);
+
+        // use a linear layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ViewAdapter = new MyAdapter();
+        recyclerView.setAdapter(ViewAdapter);
+
+//        if (savedInstanceState != null)
+//        {
+//            input.setText(savedInstanceState.getString("in"));
+//            //recyclerView (savedInstanceState.getString("out"));
+//            ViewAdapter.setData(savedInstanceState.getStringArrayList("list"));
+//            ViewAdapter.supportConfigurationChange();
+//        }
+
+        button.setOnClickListener(this);
+
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("in", input.getText().toString());
+        outState.putStringArrayList("list", ViewAdapter.getData());
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        input.setText(savedInstanceState.getString("in"));
+        ViewAdapter.myMessages = (savedInstanceState.getStringArrayList("list"));
+        ViewAdapter.supportConfigurationChange();
+    }
+
+    @Override
+    public void onClick(View view) {
+        String msg;
+        msg = input.getText().toString();
+        input.setText("");
+        //incase they tried to add empty text
+        if (msg.length() == 0 ){
+            Toast.makeText(getApplicationContext(), "NO OH, OH, no empty text allowed"
+                    , Toast.LENGTH_LONG).show();
+            return;
+        }
+        ViewAdapter.addMsg(msg);
+
+    }
+}
---
+++ app/src/main/java/com/example/selfchat_rv/MyAdapter.java
@@ -0,0 +1,66 @@
+package com.example.selfchat_rv;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        TextView textView;
+        MyViewHolder(TextView v) {
+            super(v);
+            textView = ((TextView) itemView.findViewById(R.id.plain_text_output));
+        }
+
+        public void display(String message) {
+            textView.setText(message);
+        }
+
+    }
+
+    ArrayList<String> myMessages = new ArrayList<String>();
+
+    public MyAdapter(){
+        myMessages = new ArrayList<String>();
+    }
+
+    public void supportConfigurationChange() {
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.interior, parent, false);
+
+        MyViewHolder viewHolderh = new MyViewHolder(v);
+        return viewHolderh;
+    }
+
+
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.display(myMessages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return myMessages.size();
+    }
+
+    public void addMsg(String msg) {
+        myMessages.add(msg);
+        notifyDataSetChanged();
+
+    }
+
+    public ArrayList<String> getData() {
+        return myMessages;
+    }
+}
