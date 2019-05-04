3d728479e3e57abfe03fee921e01f34884eaede4
sineen <sineen.nawatha@mail.huji.ac.il>
2019-05-04 23:57:29 +0300

    not using room lets see if my computer wont crash now

--- app/src/main/java/com/example/selfchat_rv/MainActivity.java
+++ app/src/main/java/com/example/selfchat_rv/MainActivity.java
@@ -5,25 +5,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity implements MyAdapter.recItemOnLongClick{
 
-public class MainActivity extends AppCompatActivity implements MyUtil.MsgClickCallback {//implements View.OnClickListener{
+    public static final String DATA_SIZE = "Datasize";
+    public static final String DATA_LIST = "sent";
 
     EditText input;
     RecyclerView recyclerView;
+    MyAdapter ViewAdapter;
     Button button;
 
-    MyDatabase room;
-    MyUtil.MsgAdapter adapter = new MyUtil.MsgAdapter();
-    ArrayList<Messege> msgs = new ArrayList<>();
+    SharedPreferences MySharedPrefrance;
+    SharedPreferences.Editor MyEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,78 +35,80 @@ public class MainActivity extends AppCompatActivity implements MyUtil.MsgClickCa
         input = (EditText) findViewById(R.id.plain_text_input);
         button = (Button) findViewById(R.id.button);
 
-        room = MyDatabase.getDatabase(this);
-        msgs.addAll(room.msgDao().getAllMsgs());
 
-        Log.v("size of messages list",Integer.toString(msgs.size()));
-
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         // use a linear layout manager
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        adapter.callback = this;
-        adapter.submitList(msgs);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        int size = MySharedPrefrance.getInt(DATA_SIZE, 0);
+        MySharedPrefrance = PreferenceManager.getDefaultSharedPreferences(this);
+        MyEditor = MySharedPrefrance.edit();
+        ViewAdapter = new MyAdapter(size , MySharedPrefrance, MyEditor);
+        ViewAdapter.setClickListener((MyAdapter.recItemOnLongClick) this);
+        recyclerView.setAdapter(ViewAdapter);
+
+        if(size != 0 ) {
+            ViewAdapter.loading();
+        }
+
+
+        if (savedInstanceState != null)
+        {
+            String previusMessages = (savedInstanceState.getString("in"));
+            ViewAdapter.setData(savedInstanceState.getStringArrayList("list"));
+            input.setText(previusMessages);
+            ViewAdapter.supportConfigurationChange();
+        }
+
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                String text;
-                text = input.getText().toString();
-                Messege msg = new Messege(text);
+            public void onClick (View view){
+                String msg;
+                msg = input.getText().toString();
+                input.setText("");
                 //incase they tried to add empty text
-                if (text.length() == 0 ){
+                if (msg.length() == 0) {
                     Toast.makeText(getApplicationContext(), "NO OH, OH, no empty text allowed"
                             , Toast.LENGTH_LONG).show();
                     return;
-                } else {
-                    input.setText("");
-                    msgs.add(msg);
-                    room.msgDao().insert(msg);
-                    adapter.submitList(msgs);
-                    adapter.notifyDataSetChanged();
                 }
+                ViewAdapter.addMsg(msg);
+
             }
         });
-
     }
 
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList("messages", (ArrayList<? extends Parcelable>) msgs);
-    }
-
-    public void onRestoreInstanceState(Bundle state) {
-        super.onRestoreInstanceState(state);
-        msgs = state.getParcelableArrayList("messages");
-        adapter.submitList(msgs);
-        adapter.notifyDataSetChanged();
+        outState.putString("in", input.getText().toString());
+        outState.putStringArrayList("list", ViewAdapter.getData());
     }
 
 
     @Override
-    public void onMsgClick(final Messege msg) {
+    public void itemLongClick(View view, int position) {
+        new AlertDialog.Builder(this)
+                .setTitle("Are you sure you wana delete it???")
+                .setMessage("this will be the point of no return ")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position) {
+                        ViewAdapter.deleteMessage(position);
+                        Toast.makeText(getApplicationContext(),
+                                "message is Gone never to be back again", Toast.LENGTH_LONG).show();
 
-        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case DialogInterface.BUTTON_POSITIVE:
-                        ArrayList<Messege> msgCopy = new ArrayList<>(msgs);
-                        msgCopy.remove(msg);
-                        room.msgDao().delete(msg);
-                        msgs = msgCopy;
-                        adapter.submitList(msgs);
-                        adapter.notifyDataSetChanged();
-                        break;
-
-                    case DialogInterface.BUTTON_NEGATIVE:
-                        break;
-                }
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
-                .setNegativeButton("No", dialogClickListener).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),
+                                "deleting process was aborted", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNeutralButton("Cancel", null)
+                .show();
     }
 }
--- app/src/main/java/com/example/selfchat_rv/MyAdapter.java
+++ app/src/main/java/com/example/selfchat_rv/MyAdapter.java
@@ -1,92 +1,133 @@
-//package com.example.selfchat_rv;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
-//
-//import java.util.ArrayList;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.DiffUtil;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-//
-////    static class MsgCallback extends DiffUtil.ItemCallback<Messege> {
-////
-////        @Override
-////        public boolean areItemsTheSame(@NonNull Messege oldItem, @NonNull Messege newItem) {
-////            return oldItem.equals(newItem);
-////        }
-////
-////        @Override
-////        public boolean areContentsTheSame(@NonNull Messege oldItem, @NonNull Messege newItem) {
-////            return oldItem.equals(newItem);
-////        }
-////    }
-////
-////
-////    interface MsgClickCallback {
-////        void onMsgClick(Messege msg);
-////    }
-////    public void setData(ArrayList<String> list) {
-////        myMessages = list;
-////    }
-//
-//    public static class MyViewHolder extends RecyclerView.ViewHolder {
-//        // each data item is just a string in this case
-//        TextView textView;
-//        MyViewHolder(View v) {
-//            super(v);
-//            textView = ((TextView) v.findViewById(R.id.plain_text_output));
-//        }
-//
-//        public void display(String message) {
-//            textView.setText(message);
-//        }
-//
-//    }
-//
-//    ArrayList<String> myMessages;
-//
-//    public MyAdapter(){
-//        myMessages = new ArrayList<String>();
-//    }
-//
-//    public void supportConfigurationChange() {
-//        notifyDataSetChanged();
-//    }
-//
-//    @Override
-//    public MyViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-//        // create a new view
-//        View v = (View) LayoutInflater.from(parent.getContext())
-//                .inflate(R.layout.interior, parent, false);
-//
-//        MyViewHolder viewHolderh = new MyViewHolder(v);
-//        return viewHolderh;
-//
-//
-//    }
-//
-//
-//    public void onBindViewHolder(MyViewHolder holder, int position) {
-//        holder.display(myMessages.get(position));
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return myMessages.size();
-//    }
-//
-//    public void addMsg(String msg) {
-//        myMessages.add(msg);
-//        notifyDataSetChanged();
-//
-//    }
-//
-//    public ArrayList<String> getData() {
-//        return myMessages;
-//    }
-//}
+package com.example.selfchat_rv;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.content.SharedPreferences;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import static com.example.selfchat_rv.MainActivity.DATA_LIST;
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public  interface  recItemOnLongClick {
+        void itemLongClick(View view, final int position);
+    }
+
+
+    private ArrayList<String> myMessages;
+    private recItemOnLongClick clicked;
+    private int DataSize;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private Gson gson;
+
+
+
+    public void setData(ArrayList<String> list) {
+        myMessages = list;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        TextView textView;
+        MyViewHolder(View v) {
+            super(v);
+            textView = ((TextView) v.findViewById(R.id.plain_text_output));
+            v.setOnLongClickListener((View.OnLongClickListener) this);
+        }
+
+        public void display(String message) {
+            textView.setText(message);
+        }
+
+        public boolean onLongClick(View v) {
+            if (clicked != null) {
+                clicked.itemLongClick(v, getAdapterPosition());
+            }
+            return true;
+        }
+    }
+
+
+    public MyAdapter( int size, SharedPreferences sp, SharedPreferences.Editor edit){
+        myMessages = new ArrayList<String>();
+        gson = new Gson();
+        DataSize = size;
+        sharedPreferences = sp;
+        editor = edit;
+    }
+
+    public void supportConfigurationChange() {
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        // create a new view
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.interior, parent, false);
+
+        MyViewHolder viewHolderh = new MyViewHolder(v);
+        return viewHolderh;
+
+
+    }
+
+
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.display(myMessages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return myMessages.size();
+    }
+
+    public void saveEditions()
+    {
+        editor.putInt(MainActivity.DATA_SIZE, DataSize);
+        String wjson = gson.toJson(myMessages);
+        editor.putString(DATA_LIST, wjson);
+        editor.apply();
+    }
+
+    public void addMsg(String msg) {
+        myMessages.add(msg);
+        DataSize ++;
+        saveEditions();
+        notifyDataSetChanged();
+
+    }
+
+    public void deleteMessage(int position) {
+        myMessages.remove(position);
+        DataSize --;
+        saveEditions();
+        notifyDataSetChanged();
+    }
+
+    public void setClickListener(recItemOnLongClick itemClick) {
+        this.clicked = itemClick;
+    }
+
+
+    public ArrayList<String> getData() {
+        return myMessages;
+    }
+
+    public void loading( ){
+        String rjson = sharedPreferences.getString(DATA_LIST, "");
+        Type type = new TypeToken<List<String>>() {
+        }.getType();
+        myMessages = gson.fromJson(rjson, type);
+    }
+}
